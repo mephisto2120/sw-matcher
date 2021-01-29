e6b1d5558584c97c36535310d1076d21e573b1fd
Mariusz Malara <m.malara@consultant.crif.com>
2021-01-29 21:06:05 +0100

    Exposing API

--- src/main/java/com/tryton/small_world/matcher/controller/CustomerController.java
+++ src/main/java/com/tryton/small_world/matcher/controller/CustomerController.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/customer")
 @RequiredArgsConstructor
